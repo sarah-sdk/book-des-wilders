6c90d0e3707a35eb90e6131338a0728ce413b29a
Sarah Smandack <sarah.smandack@gmail.com>
2024-10-10 16:09:38 +0200

    move table in other file

--- index.js
+++ index.js
@@ -1,65 +1,6 @@
-let studentWild = [
-    {
-        firstName: 'Geoffrey',
-        lastName: 'Leglise',
-        promo: 'DevWeb',
-        birthDate: '03/03/1995',
-        github:'https://github.com/gleglise',
-        linkedin:'https://fr.linkedin.com/in/geoffrey-leglise',
-        hobbies:'Jeux Videos, Musique, Tech',
-        proObjective:'Développeur Web.',
-        quote:'',
-        image: 'https://placebear.com/200/300',
-    },
-    {
-        firstName: 'Julien',
-        lastName: 'Cartron',
-        promo: 'DevWeb',
-        birthDate: '12/09/1994',
-        github:'https://github.com/Wakoz',
-        linkedin:'https://www.linkedin.com/in/julien-cartron-747559330/',
-        hobbies:'Jeux Videos,Musique',
-        proObjective:'Obtenir une alternance a la suite de mon titre',
-        quote:"",
-        image: 'https://placebear.com/200/300',
-    },
-    {
-        firstName: 'Sarah',
-        lastName: 'Smandack',
-        promo: 'DevWeb',
-        birthDate: '08/01/1997',
-        github:'https://github.com/sarah-sdk',
-        linkedin:'https://fr.linkedin.com/in/sarah-smandack-161419167',
-        hobbies:'Jeux videos, manger',
-        proObjective:'Travailler de là où je veux',
-        quote:'"C\est l\hopital qui se fiche du beurre de karité."',
-        image: 'https://placebear.com/200/300',
-    },
-    {
-        firstName: 'Marwane',
-        lastName: 'Ben Khalifa',
-        promo: 'DevWeb',
-        birthDate: '25/03/1997',
-        github:'',
-        linkedin:'',
-        hobbies:'',
-        proObjective:'',
-        quote:'',
-        image: 'https://placebear.com/200/300',
-    },
-    {
-        firstName: 'Sybill',
-        lastName: 'Gribonval',
-        promo: 'TSSR',
-        birthDate: '08/01/1992',
-        github:'https://github.com/Mirhazka',
-        linkedin:'https://www.linkedin.com/in/sybill-gribonval-0ab964165/',
-        hobbies:'Jeux vidéo & Manga',
-        proObjective:'',
-        quote:'',
-        image: 'https://placebear.com/g/200/300',
-    },
-]
+import studentWild from "./tableau.js";
+console.log(studentWild);
+
 function creatCard(student){
     const trombinoscope = document.querySelector(".trombinoscope");
     //elem carte
